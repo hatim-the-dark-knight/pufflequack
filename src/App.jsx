319959dcf88e21f0460531f119bba46df2a867b5
Farhath Sulthana <farhathsulthana2001@gmail.com>
2023-09-17 13:35:56 +0530

    update example code

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   return (
     <>
