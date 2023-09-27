711b5023716009981fadc6e1705eb2bf981c8fed
Farhath Sulthana <57259914+hatim-the-dark-knight@users.noreply.github.com>
2023-09-27 08:04:33 +0530

    update categories

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,20 +1,42 @@
 import React from 'react'
-import { Nav, Navbar, Container } from 'react-bootstrap'
+import { Stack, ImageList, ImageListItem, ImageListItemBar } from '@mui/material'
 import './Content.css'
+import data from '../../utils/categories.json'
 
 const Content = () => {
   return (
-    <div className="grid">
-        <div className="item-card">
-            <img src="public/Burger_148.png" alt="" />
+    <Stack spacing={4} cols={3} rowHeight={250} className="grid">
+      <ImageList>
+      {
+        data.map((item, i) => {
+          return(
+            <ImageListItem id={i} >
+              <img
+                srcSet={`${item.image}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                src={`${item.image}?w=248&fit=crop&auto=format`}
+                alt={item.title}
+                loading="lazy"
+              />
+              <ImageListItemBar
+                title={item.title}
+                subtitle={<span> {item.items.length}</span>}
+                position="below"
+              />
+            </ImageListItem>
+          )
+        })
+      }
+      </ImageList>
+        {/* <div className="item-card">
+            <img src="/Burger_148.png" alt="" />
             <div className="label">
                 Burger * <b>$4.99</b>
             </div>
             <button className="addButton">
                 ADD
             </button>
-        </div>
-    </div>
+        </div> */}
+    </Stack>
   )
 }
 
