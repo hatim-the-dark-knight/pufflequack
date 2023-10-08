83ba0de8fd9bf3c6512be36aeebcf5ea27ed0f8d
Farhath Sulthana <farhathsulthana2001@gmail.com>
2023-10-08 21:19:13 +0530

    Add cover

--- src/components/Cover/Cover.jsx
+++ src/components/Cover/Cover.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import './Cover.css'
+import Home from "../Home";
+
+const delay = ms => new Promise(
+    resolve => setTimeout(resolve, ms)
+);
 
 const Cover = () => {
+    const navigate = useNavigate();
+
+    const renderHome = async event => {
+        await delay(2000);
+        console.log("Now");
+        navigate("/pufflequack/easydineouts");
+    }
+
+    useEffect(() => {
+        renderHome();
+    }, []);
+
     return(
         <div className="cover">
-            easydineouts
+            <div className="cover-title">
+                easydineouts
+            </div>
         </div>
     )
 }
