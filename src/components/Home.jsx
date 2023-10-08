fca4d3ef9def69d689851573ec2f625a381e77fa
Farhath Sulthana <farhathsulthana2001@gmail.com>
2023-10-08 10:45:27 +0530

    Update test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,15 +3,16 @@ import Header from './common/Header/Header'
 import CategoryMenu from './CategoryMenu/CategoryMenu'
 import HomeContent from './HomeContent/HomeContent'
 import SearchBar from './SearchBar/SearchBar'
+import '../App.css'
 
 const Home = () => {
   return (
-    <>
+    <div className='home'>
         <Header/>
         <SearchBar/>
         <CategoryMenu/>
         <HomeContent/>
-    </>
+    </div>
   )
 }
 
