340d290e27dd4da5e6033075bf01cc8830702373
Farhath Sulthana <farhathsulthana2001@gmail.com>
2023-10-10 08:28:29 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,15 +1,24 @@
-import { Telegraf } from 'telegraf'
+
+const { Markup, Telegraf } = require('telegraf');
+
+const { message } = require('telegraf/filters');
 
 const bot = new Telegraf("6603170388:AAFOgVDjefLYiha8q1igeMQmecWXrFsV7F8")
 
-bot.on(message('text'), async (ctx) => {
+bot.start(async (ctx) => {
   // Using context shortcut
-  await ctx.reply(`Welcome! Book your table now and savor a delightful dining experience with easydineouts!`, {
-    reply_markup: {
-        inline_keyboard: [[{ text: "Book Table", web_app: {url: "https://hatim-the-dark-knight.github.io/pufflequack/"}}]]
-    }
-  })
+  await ctx.reply(`Welcome! Book your table now and savor a delightful dining experience with easydineouts!`, Markup.keyboard([Markup.button.webApp("Book Table", "https://hatim-the-dark-knight.github.io/pufflequack/")]).resize(),);
+
 })
+
+bot.on(message, async (ctx) => {
+  const data = JSON.parse(ctx.message.web_app_data.data);
+  const { date, seats, time_slot } = data;
+  // Reply to the user with "Your data was:"
+  await ctx.reply(`Table booked for ${date} from ${time_slot} for ${seats} people. Enjoy!`);
+
+
+});
 
 bot.launch()
 
